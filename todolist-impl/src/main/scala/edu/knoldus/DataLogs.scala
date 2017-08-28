4a1906a3b3200f1e08b0d6f594e532c422d5f53a
Neelaksh Chauhan <neelaksh.chauhan@knoldus.in>
2017-08-28 16:38:23 +0530

    removed map in favor of onComplete in the actor

--- todolist-impl/src/main/scala/edu/knoldus/DataLogs.scala
+++ todolist-impl/src/main/scala/edu/knoldus/DataLogs.scala
@@ -3,7 +3,9 @@ package edu.knoldus
 import akka.actor.{Actor, Props}
 import edu.knoldus.DataLogs.LogAll
 import play.api.Logger
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 /**
   * Created by Neelaksh on 27/8/17.
@@ -11,10 +13,12 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class DataLogs(externalService: ExternalService) extends Actor {
   override def receive: Receive = {
     case LogAll =>
-      externalService.getToDoList().invoke().map {
-        data =>
+      externalService.getToDoList().invoke().onComplete {
+        case Success(data) =>
           val formattedData = data.foldLeft("")((acc, todo) => acc + todo.toString + "\n")
           Logger.info(formattedData)
+        case Failure(ex) =>
+          Logger.error(ex.getStackTrace.toString)
       }
   }
 }
